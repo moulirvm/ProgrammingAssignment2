9e9866847419566b86d8075565b39a90ca768c52
moulirvm <mouli.rvm@gmail.com>
2014-07-23 15:46:47 -0400

    modified

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#' This function creates a list of functions to 
+#' cache matrix and inverse matrix
+#' 
+#' @return the list of cache
+#' @examples 
+#' x <- rbind(c(1,2), c(3,4))
+#' y <- makeCacheMatrix(x)
+#' 
+makeCacheMatrix <- function(x = numeric()) {
+  #Initializing the inverse matrix
+  inv <- NULL
+  
+  #Set the value of matrix for caching
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #Return the value of cached matrix
+  get <- function() {
+    x
+  }
+  
+  #Set the value of inverse matrix for caching
+  setinverse <- function(inverse) {
+    inv <<- inverse
+  }
+  
+  #Return the value of cached inverse matrix
+  getinverse <- function() {
+    inv
+  }
+  
+  #Return the list of functions used as cache
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+#' This function calculates matrix inverse.
+#' It looks into cache and if available returns inverse matrix 
+#' or computes inverse matrix if not available in cache
+#' 
+#' @return the inverse matrix
+#' @examples 
+#' x <- makeCacheMatrix(rbind(c(1,2), c(3,4)))
+#' cacheSolve(x)
+#' 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Extract inverse value from the list
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # Calculate inverse if no cache is available
+  data <- x$get()
+  inv <- solve(data, ...)
+  
+  # Set inverse matrix in the list
+  x$setinverse(inv)
+  
+  # Return inverse matrix
+  inv
 }
+
+
